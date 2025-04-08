bcc83e3fe11c12a0c2030cce3e3993e1c2304fdc
nica <lamistage@gmail.com>
2025-04-08 17:58:36 +0700

    routes to favorites added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AddImageComponent } from './components/add-image/add-image.component';
 import { AuthGuard } from './services/auth.guard';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 
 export const routes: Route[] = [
   { path: '', redirectTo: 'gallery', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
   { path: 'add-image', component: AddImageComponent, canActivate: [AuthGuard] },
   { path: 'gallery', component: GalleryComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] }
 ];
